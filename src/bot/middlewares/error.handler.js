4743b526b5724a53db5839e30f6ea81368da66c2
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2024-09-20 13:38:11 -0400

    Feature: Se ajuste nombre de la funcion que maneja los errores en log

--- src/bot/middlewares/error.handler.js
+++ src/bot/middlewares/error.handler.js
@@ -1,8 +1,8 @@
-function logErrors (context, error) {
+function logError (context, error) {
     console.error(error);
     context.reply(`Lo sentimos! Ha ocurrido un error. Revisa que hayas enviado el comando y los parámetros correctos`);
 }
 
 module.exports = {
-    logErrors
+    logError
 }
